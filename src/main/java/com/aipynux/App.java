dcfbbfbad92cf5e6e181c6276ac1e7e375cc6edb
hgneer <hgneer@gmail.com>
2022-01-12 10:13:40 +0800

    review

---
+++ src/main/java/com/aipynux/App.java
@@ -0,0 +1,13 @@
+package com.aipynux;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
